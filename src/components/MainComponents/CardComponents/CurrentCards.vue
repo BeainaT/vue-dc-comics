9d8606fb262780bb076d71a93bcb410e5d39f584
BeainaT <tania.df@hotmail.it>
2022-06-06 16:15:25 +0200

    added main components + bug on v-for

---
+++ src/components/MainComponents/CardComponents/CurrentCards.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card_box">
+        <img :src="img" alt="">
+        <h3>{{title}}</h3> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CurrentCards",
+    props: {
+        img: String,
+        title: String,
+    },
+}
+</script>
+
+<style>
+
+</style>
--- src/components/baseMain.vue
+++ src/components/baseMain.vue
@@ -1,15 +1,18 @@
 <template>
   <main>
     <MainHero />
+    <MainCards />
   </main>
 </template>
 
 <script>
 import MainHero from "./MainComponents/MainHero.vue";
+import MainCards from "./MainComponents/MainCards.vue";
 export default {
   name: "BaseMain",
   components: {
     MainHero,
+    MainCards
   },
 };
 </script>
